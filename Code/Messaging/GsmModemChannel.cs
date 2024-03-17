8082b95ab0463fd8703c5792c2a814a4c228b5f4
Sepehr Safari <sepehr.safari8731@gmail.com>
2024-03-17 20:51:09 +0000

    fix typo

--- Code/Messaging/GsmModemChannel.cs
+++ Code/Messaging/GsmModemChannel.cs
@@ -2,6 +2,6 @@ public class GsmModemChannel: IMessenger
 {
     public void sendMessage(string str)
     {
-        Console.WriteLine("GsmModel: " + str);
+        Console.WriteLine("GsmModem: " + str);
     }
 }
