7bdaca663280805f201beb18ff8a80ad8cddb667
Sepehr Safari <sepehr.safari8731@gmail.com>
2024-03-17 21:12:45 +0000

    Add validation

--- Code/DTOs/InvoiceDto.cs
+++ Code/DTOs/InvoiceDto.cs
@@ -6,6 +6,7 @@ public class InvoiceDto
     public required string BuyerName { get; set; }
 
     [Required(ErrorMessage = "PhoneNumber is required")]
+    [RegularExpression(@"^\+98\d{10}$", ErrorMessage = "Invalid phone number")]
     public required string PhoneNumber { get; set; }
 
     [Required(ErrorMessage = "Items is required")]
--- Code/DTOs/ItemDto.cs
+++ Code/DTOs/ItemDto.cs
@@ -6,8 +6,10 @@ public class ItemDto
     public required string ProductName { get; set; }
 
     [Required(ErrorMessage = "Count is required")]
+    [Range(1, int.MaxValue, ErrorMessage = "Count must be a positive integer")]
     public int Count { get; set; }
 
     [Required(ErrorMessage = "UnitPrice is required")]
+    [Range(1, int.MaxValue, ErrorMessage = "Unit price must be a positive integer")]
     public int UnitPrice { get; set; }
 }
